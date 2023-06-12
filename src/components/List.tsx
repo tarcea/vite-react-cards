20efc3635781affbb13efad02cf32cafec0af3a0
tarcea <tarcea@gmail.com>
2023-06-12 13:56:44 +0200

    som trials with card design

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,7 +2,7 @@ import Card from './Card';
 
 const List = ({ data }: { data: Card[] }) => {
   return (
-    <div className='grid sm:grid-cols-4 grid-cols-1 grid-flow-row gap-4 mx-auto'>
+    <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 grid-flow-row gap-4 mx-auto'>
       {data.map((card: Card) => (
         <Card card={card} key={card.id} />
       ))}
