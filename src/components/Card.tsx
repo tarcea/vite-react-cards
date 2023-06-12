6f733ad782e516bc6082cd725a84fa8d7365f76f
tarcea <tarcea@gmail.com>
2023-06-12 16:21:42 +0200

    fixed the grid of cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,18 @@
 const Card = ({ card }: { card: Card }) => {
   const { title, image, description } = card;
-
+  const short = (string: string, length: number) => {
+    let str = string;
+    if (str.length > length) {
+      str = str.substring(0, length) + '...';
+    }
+    return str;
+  };
   return (
     <div className='bg-gray-100'>
-      <div className=''>
-        <h1 className='title-font sm:text-2xl text-xl  mb-3'>{title}</h1>
-        <img src={image} />
-        <p className='mb-3'>{description}</p>
-      </div>
+      <h1 className='text-xl text-left'>{title}</h1>
+      <img src={image} className='w-1/2 m-auto' />
+      <p className='mb-3 text-left'>{short(description, 100)}</p>
+      <div className='flex content-around'></div>
     </div>
   );
 };
