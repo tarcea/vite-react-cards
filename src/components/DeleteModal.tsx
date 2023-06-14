c95cdccba549fa971a4264731524ac355ba80ee9
tarcea <tarcea@gmail.com>
2023-06-14 20:51:28 +0200

    styled delete modal

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -1,3 +1,5 @@
+import { FiCheck, FiX } from 'react-icons/fi';
+
 const DeleteModal = ({
   setModal,
   reallyRemoveCard,
@@ -11,9 +13,10 @@ const DeleteModal = ({
     setModal(false);
     reallyRemoveCard();
   };
+
   return (
     <div>
-      <div className='fixed top-1/4 left-1 lg:left-1/4 lg:h-1/2 lg:w-1/2 bg-red-400/80 rounded-lg z-40 w-full ml-0.5 '>
+      <div className='fixed top-1/4 left-1 lg:left-1/4 lg:h-1/2 lg:w-1/2 bg-violet-200/80 rounded-lg z-40 w-full ml-0.5 '>
         <div className='text-center text-black relative'>
           <p className='mt-10 p-2 text-xl'>
             Do you really want to delete this card?{' '}
@@ -21,18 +24,24 @@ const DeleteModal = ({
           <em className='p-2 text-l'>{card.title}</em>
         </div>
         <div className='flex justify-around mt-10 lg:mt-40'>
-          <button onClick={really} className='p-2'>
-            YES
-          </button>
-          <button onClick={() => setModal(false)} className='p-2'>
-            NO
-          </button>
+          <div
+            onClick={really}
+            className='p-2 text-5xl text-red-700 cursor-pointer'
+          >
+            <FiCheck />
+          </div>
+          <div
+            onClick={() => setModal(false)}
+            className='p-2 text-5xl text-green-700 cursor-pointer'
+          >
+            <FiX />
+          </div>
         </div>
         <button
           onClick={() => setModal(false)}
-          className='absolute top-5 right-5'
+          className='absolute top-5 right-5 text-2xl cursor-pointer'
         >
-          Close
+          <FiX />
         </button>
       </div>
     </div>
