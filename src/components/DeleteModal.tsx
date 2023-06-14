26b0d6c0ae2eb1d618c600eb61c6c495c16c906e
tarcea <tarcea@gmail.com>
2023-06-14 15:17:02 +0200

    added modal for relly want to delete?

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -13,18 +13,18 @@ const DeleteModal = ({
   };
   return (
     <div>
-      <div className='fixed top-1/4 left-1 lg:left-1/4 h-1/2 lg:w-1/2 bg-red-400/80 rounded-lg z-40 w-80'>
+      <div className='fixed top-1/4 left-1 lg:left-1/4 h-1/2 lg:w-1/2 bg-red-400/80 rounded-lg z-40 w-full'>
         <div className='text-center text-black relative'>
-          <div>
-            <button onClick={really} className='p-2'>
-              YES
-            </button>
-            <button onClick={() => setModal(false)} className='p-2'>
-              NO
-            </button>
-          </div>
           <p className='mt-20 p-2'>Do you really want to delete this card? </p>
           <em className='p-2'>{card.title}</em>
+        </div>
+        <div className='flex justify-around mt-20'>
+          <button onClick={really} className='p-2'>
+            YES
+          </button>
+          <button onClick={() => setModal(false)} className='p-2'>
+            NO
+          </button>
         </div>
         <button
           onClick={() => setModal(false)}
