a76f1749100cb2f3cf920087787f79b6b2d2c421
tarcea <tarcea@gmail.com>
2023-06-14 08:49:52 +0200

    styled the card menu

--- src/components/ButtonDetails.tsx
+++ src/components/ButtonDetails.tsx
@@ -1,20 +1,19 @@
 import TextButton from './TextButton';
 
 const ButtonDetails = ({ cardUrl }: { cardUrl: string }) => {
-  const visitUrl = (e: any) => {
-    // ChangeEvent<HTMLInputElement>
-    e.stopPropagation();
-    window.open(cardUrl, '_blank');
-  };
-
-  const removeCard = (e: any) => {
-    e.stopPropagation();
+  const removeCard = () => {
     console.log('remove card');
   };
 
   return (
-    <div className='absolute top-14 right-1 z-40 w-20 h-20 bg-white flex flex-wrap justify-center content-around rounded drop-shadow-xl'>
-      <div onClick={visitUrl} className='hover:bg-indigo-100 w-full p-1'>
+    <div
+      className='absolute top-14 right-1 z-40 w-20 h-20 bg-white flex flex-wrap justify-center content-around rounded drop-shadow-xl'
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div
+        onClick={() => window.open(cardUrl, '_blank')}
+        className='hover:bg-indigo-100 w-full p-1'
+      >
         <TextButton text='visitUrl' />
       </div>
       <div onClick={removeCard} className='hover:bg-indigo-100 w-full p-1'>
