e7f1c310de74a646005564630bada5c3d69045b6
tarcea <tarcea@gmail.com>
2023-06-14 21:42:04 +0200

    readme added

--- src/components/List.test.tsx
+++ src/components/List.test.tsx
@@ -1,22 +1,22 @@
-const data = [
-  {
-    id: 1,
-    title: 'Develop & test React components in isolation',
-    image:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--tQrhntbo--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/mwpnd4ztvt2yme2zyipl.jpg',
-    description:
-      "Writing front-end code is easy. But writing reliable front-end code? That's tough. Modern UIs are...",
-    url: 'https://dev.to/dhaiwat10/develop-test-react-components-in-isolation-3714',
-  },
-  {
-    id: 2,
-    title: 'AutoDraw',
-    image: 'http://www.autodraw.com/assets/images/autodraw-shareimage.png',
-    description:
-      'Fast drawing for everyone. AutoDraw pairs machine learning with drawings from talented artists to help you draw stuff fast.',
-    url: 'https://www.autodraw.com/',
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     title: 'Develop & test React components in isolation',
+//     image:
+//       'https://res.cloudinary.com/practicaldev/image/fetch/s--tQrhntbo--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/mwpnd4ztvt2yme2zyipl.jpg',
+//     description:
+//       "Writing front-end code is easy. But writing reliable front-end code? That's tough. Modern UIs are...",
+//     url: 'https://dev.to/dhaiwat10/develop-test-react-components-in-isolation-3714',
+//   },
+//   {
+//     id: 2,
+//     title: 'AutoDraw',
+//     image: 'http://www.autodraw.com/assets/images/autodraw-shareimage.png',
+//     description:
+//       'Fast drawing for everyone. AutoDraw pairs machine learning with drawings from talented artists to help you draw stuff fast.',
+//     url: 'https://www.autodraw.com/',
+//   },
+// ];
 
 it('render a list of items', () => {
   // render(<List data={data} />);
