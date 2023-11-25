b8db7d498e575b0b1e9a1acf4e31cba63c6cdf14
tarcea <tarcea@gmail.com>
2023-11-25 21:11:34 +0100

    bold font on cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -34,25 +34,25 @@ const Card = ({
 
   return (
     <div
-      className='bg-violet-50 flex flex-wrap items-center justify-between rounded-lg h-20 w-full relative hover:shadow-xlg hover:shadow-indigo-800/60 shadow-lg shadow-indigo-400/40 cursor-pointer'
+      className="bg-violet-50 flex flex-wrap items-center justify-between rounded-lg h-20 w-full relative hover:shadow-xlg hover:shadow-indigo-800/60 shadow-lg shadow-indigo-400/40 cursor-pointer"
       onClick={() => window.open(url, '_blank')}
     >
       <div
-        className='w-24  h-full rounded-l-lg bg-center bg-no-repeat bg-cover'
+        className="w-24 h-full rounded-l-lg bg-center bg-no-repeat bg-cover"
         style={{ backgroundImage: `url(${image || '/no-image-png.webp'})` }}
       ></div>
 
-      <div className=''>
-        <p className='text-md font-thin text text-left absolute top-3 left-32'>
+      <div className="">
+        <p className="text-md font-semibold text text-left absolute top-3 left-32">
           {short(displayUrl.host, 20)}
         </p>
-        <p className='text-xs text text-left font-bold absolute bottom-5 left-32'>
+        <p className="text-xs text text-left font-extrabold absolute bottom-5 left-32">
           {short(title, 20)}
         </p>
       </div>
       <div
-        className='flex flex-nowrap items-center justify-center lg:cursor-pointer w-10 h-10 hover:rounded-full hover:bg-violet-200'
-        title='Details'
+        className="flex flex-nowrap items-center justify-center lg:cursor-pointer w-10 h-10 hover:rounded-full hover:bg-violet-200"
+        title="Details"
         onClick={handleDetailsButton}
       >
         <BsThreeDotsVertical />
